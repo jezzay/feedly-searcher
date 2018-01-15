0395e829e65a82d130095114ca14e7fe91b45117
Jeremy <jezzay@users.noreply.github.com>
2018-01-16 00:46:13 +1100

    make multiple http requests to feedly api; wait on channels

--- feedly/stream.go
+++ feedly/stream.go
@@ -4,21 +4,24 @@ import (
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
+	"fmt"
 )
 
 type FeedContent struct {
-	Title string `json:"title"`
+	Title string     `json:"title"`
 	Items []FeedItem `json:"items"`
 }
 
 type FeedItem struct {
-	Title string `json:"title"`
+	Title    string `json:"title"`
 	OriginId string `json:"originId"`
 }
 
-func StreamContents(streamId string) FeedContent  {
+func StreamContent(streamId string, ch chan<- FeedContent) {
 	// TODO: proper http resp code handling
-	resp, err := http.Get("https://cloud.feedly.com/v3/streams/contents?streamId=" + streamId)
+	url := "https://cloud.feedly.com/v3/streams/contents?streamId=" + streamId
+	fmt.Printf("Requesting %s \n", url)
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
@@ -27,8 +30,17 @@ func StreamContents(streamId string) FeedContent  {
 
 	var feed FeedContent
 	err = json.Unmarshal(respBytes, &feed)
+
+	fmt.Printf("Response %v \n", feed)
+
 	if err != nil {
 		panic(err)
 	}
-	return feed
+	ch <- feed
+}
+
+func StreamContents(streamIds []string, ch chan<- FeedContent) {
+	for _, id := range streamIds {
+		go StreamContent(id, ch)
+	}
 }
--- main.go
+++ main.go
@@ -3,11 +3,58 @@ package main
 import (
 	"github.com/jezzay/xkcd/feedly"
 	"fmt"
+	"time"
 )
 
 func main() {
+	feed := make(chan feedly.FeedContent)
+	feeds := make(chan feedly.FeedContent)
+	done := make(chan bool, 2)
 
-	contents := feedly.StreamContents("feed/http://xkcd.com/rss.xml")
-	fmt.Printf("%+v", contents)
+	// Issue multiple requests and wait
+	feedly.StreamContents([]string{"feed/http://xkcd.com/rss.xml", "feed/http://xkcd.com/rss.xml"}, feeds)
 
+	// Single request
+	go feedly.StreamContent("feed/http://xkcd.com/rss.xml", feed)
+
+	received := 0
+	completedChannels := 0
+
+	// non blocking select
+	for {
+		select {
+		case content := <-feed:
+			fmt.Printf("\n Result from single request =  %+v \n", content)
+			done <- true
+		case contents := <-feeds:
+			fmt.Printf("\n Result number %v from multiple requests:  %+v \n", received, contents)
+			received++
+			if received == 2 {
+				done <- true
+			}
+		case <-done:
+			completedChannels++
+			if completedChannels == 2 {
+			fmt.Printf("All channels have completed; exiting")
+				return
+			}
+
+		default:
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+
+	// blocking read from channels; both the single and range will block
+
+	//content := <-feed
+	//fmt.Printf("\n Result from single request =  %+v \n", content)
+	//
+	//received := 0
+	//for content := range feeds {
+	//	fmt.Printf("\n Result number %v from multiple requests:  %+v \n", received, content)
+	//	received++
+	//	if received == 2 {
+	//		close(feeds)
+	//	}
+	//}
 }
